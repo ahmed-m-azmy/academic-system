bff4b2b6ecec56bcc7f48462acd1ba7c9b1f8828
Ahmed Azmy <aazmy@hotmail.com>
2026-05-20 22:11:23 +0300

    fix supabase crash check

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,15 @@
+
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+// ✅ حماية ضد القيم الفاضية
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Supabase ENV variables missing!");
+}
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  supabaseUrl,
+  supabaseKey
 );
