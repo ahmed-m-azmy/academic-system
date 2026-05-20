da2e6b1a6dd8ed1a3d8dfc83aab1133f06562b08
Ahmed Azmy <aazmy@hotmail.com>
2026-05-20 22:53:03 +0300

    hard isolate App crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+function App() {
+  return <div>TEST ✅🔥</div>;
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
